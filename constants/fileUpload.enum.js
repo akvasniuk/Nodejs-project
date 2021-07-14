cfde6cec7d0be29e6452cd65f731e25b62e149db
akvasnuk1 <kvasnukandriy11@gmail.com>
2021-07-14 10:01:10 +0300

    Nodejs-project

---
+++ constants/fileUpload.enum.js
@@ -0,0 +1,12 @@
+module.exports = {
+  PHOTO_MAX_SIZE: 2 * 1024 * 1024, // 2MB
+
+  PHOTOS_MIMETYPES: [
+    'image/gif',
+    'image/jpeg',
+    'image/pjpeg',
+    'image/png',
+    'image/tiff',
+    'image/webp'
+  ]
+};
---
+++ middlewars/file.middleware.js
@@ -0,0 +1,58 @@
+const {
+  fileUploadEnum: {
+    PHOTOS_MIMETYPES,
+    PHOTO_MAX_SIZE
+  },
+  statusCode
+} = require('../constants');
+const { ErrorHandler, errorMessage } = require('../error');
+
+module.exports = {
+  checkFiles: (req, res, next) => {
+    try {
+      if (req.files) {
+        const files = Object.values(req.files);
+
+        const photos = [];
+
+        for (const file of files) {
+          const { name, size, mimetype } = file;
+
+          if (PHOTOS_MIMETYPES.includes(mimetype)) {
+            if (size > PHOTO_MAX_SIZE) {
+              // eslint-disable-next-line max-len
+              throw new ErrorHandler(statusCode.FILE_TOO_BIG, errorMessage.FILE_SIZE_IS_TOO_LARGE.message(name), errorMessage.FILE_SIZE_IS_TOO_LARGE.code);
+            }
+
+            photos.push(file);
+          } else {
+            // eslint-disable-next-line max-len
+            throw new ErrorHandler(statusCode.INVALID_FORMAT, errorMessage.INVALID_FORMAT.message, errorMessage.INVALID_FORMAT.code);
+          }
+        }
+
+        req.photos = photos;
+      }
+
+      next();
+    } catch (e) {
+      next(e);
+    }
+  },
+
+  checkAvatar: (req, res, next) => {
+    try {
+      if (req.photos) {
+        if (req.photos.length > 1) {
+          throw new ErrorHandler(statusCode.BAD_REQUEST, errorMessage.JUST_ONE_PHOTO.message, errorMessage.JUST_ONE_PHOTO.code);
+        }
+
+        [req.avatar] = req.photos;
+      }
+
+      next();
+    } catch (e) {
+      next(e);
+    }
+  }
+};
